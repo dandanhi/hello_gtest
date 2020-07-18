012232eda77db0a7c35b7b722a9f3c44d420a9be
sungyong <sungyong@gmail.com>
2020-07-18 16:36:41 +0900

    add hexbytestring to int test case

---
+++ hex.c
@@ -0,0 +1,41 @@
+#include <malloc.h>
+#include <string.h>
+#include "hex.h"
+
+int16_t *hex2int16(char *string)
+{
+  if (string == NULL)
+    return NULL;
+
+  size_t slength = strlen(string);
+  if ((slength % 4) != 0) // must be even
+    return NULL;
+
+  size_t dlength = slength / 4;
+  int16_t *data = (int16_t *)malloc(dlength);
+  memset(data, 0, dlength);
+
+  size_t index = 0;
+  while (index < slength)
+  {
+    char c = string[index];
+    int value = 0;
+    if (c >= '0' && c <= '9')
+      value = (c - '0');
+    else if (c >= 'A' && c <= 'F')
+      value = (10 + (c - 'A'));
+    else if (c >= 'a' && c <= 'f')
+      value = (10 + (c - 'a'));
+    else
+    {
+      free(data);
+      return NULL;
+    }
+    int shift = 0;
+    shift = 16 - (index + 1) % 4 * 4;
+    shift = (shift == 16) ? 0 : shift;
+    data[(index / 4)] += value << shift;
+    index++;
+  }
+  return data;
+}
---
+++ hex.h
@@ -0,0 +1,14 @@
+#include <stddef.h>
+
+#if 0
+#define NULL 0
+typedef unsigned int     size_t;
+#endif 
+
+typedef signed char        int8_t;
+typedef short              int16_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+
+
+int16_t *hex2int16(char *string);
