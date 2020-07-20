d664f793dd6d7228552903cf5cfc01fe7bafa975
sungyong <sungyong@gmail.com>
2020-07-20 13:06:09 +0900

    add hex test case

--- hello_test.cc
+++ hello_test.cc
@@ -1,8 +1,18 @@
 #include "gtest/gtest.h"
 #include "add.h"
+#include "hex.h"
 
 namespace {
     TEST(MyTest, Add) {
         EXPECT_EQ(2,add(1,1));
+    }
+
+    TEST(HexTest, hexStringToInt16) {
+        int16_t *res = hex2int16("0000000000010000");
+        EXPECT_EQ(0, res[0]);
+        EXPECT_EQ(0, res[1]);
+        EXPECT_EQ(1, res[2]);
+        EXPECT_EQ(0, res[3]);
+        if (res) free(res);
     }
 }
